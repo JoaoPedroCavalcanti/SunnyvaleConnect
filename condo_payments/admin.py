c267428882cbbb662f437614a905e9d27998a546
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-09-10 16:47:00 -0300

    CRUD of condo_payments created and the end point for admin to set paid to payments

--- condo_payments/admin.py
+++ condo_payments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from condo_payments.models import CondoPaymentModel
 
-# Register your models here.
+class CondoPaymentAdmin(admin.ModelAdmin):
+    ...
+    
+admin.site.register(CondoPaymentModel)
